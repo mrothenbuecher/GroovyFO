ecce930334d0b9e63818612956e6c954c5c2d438
Michael Kürbis <michael.kuerbis@gmx.de>
2015-01-06 10:54:54 +0100

    Init

---
+++ src/de/finetech/groovy/AbasBaseScript.java
@@ -0,0 +1,68 @@
+package de.finetech.groovy;
+
+import de.abas.eks.jfop.remote.EKS;
+import de.abas.eks.jfop.remote.FO;
+import groovy.lang.Script;
+
+/**
+ * 
+ * @author Michael Kürbis, Finetech GmbH & Co. KG
+ *
+ */
+public abstract class AbasBaseScript extends Script {
+
+	public String h(String varName){
+		return EKS.Hvar(varName);
+	}
+	
+	public String m(String varName){
+		return EKS.Mvar(varName);
+	}
+
+	public String d(String varName){
+		return EKS.Dvar(varName);
+	}
+	
+	public String g(String varName){
+		return EKS.Gvar(varName);
+	}
+	
+	public boolean hole(String cmd){
+		return EKS.hole(cmd);
+	}
+	
+	public boolean lade(String cmd){
+		return EKS.lade(cmd);
+	}
+	
+	public boolean dazu(String cmd){
+		return EKS.dazu(cmd);
+	}
+	
+	public void mache(String cmd){
+		 FO.mache(cmd);
+	}
+	
+	public void addZeile(){
+		 FO.mache("maske zeile +O");
+	}
+	
+	public void removeZeile(){
+		 FO.mache("maske zeile -O");
+	}
+	
+	public void bringe(String cmd){
+		EKS.bringe(cmd);
+	}
+	
+	public boolean mehr(){
+		String mehr = EKS.Gvar("mehr");
+		//FIXME Sprachunterstützung
+		return mehr != null && (mehr.toLowerCase().equals("ja") || mehr.toCharArray().equals("true"));
+	}
+	
+	public void println(String cmd){
+		EKS.println(cmd);
+	}
+
+}
---
+++ src/de/finetech/groovy/ScriptExcecuter.java
@@ -0,0 +1,69 @@
+package de.finetech.groovy;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
+
+import de.abas.eks.jfop.FOPException;
+import de.abas.eks.jfop.remote.FO;
+import de.abas.eks.jfop.remote.FOPRunnable;
+
+/**
+ *  @author Michael Kürbis, Finetech GmbH & Co. KG
+ *  
+ *  JFOP erwartet als zweiten Parameter das Groovyscript welches ausgeführt werden soll
+ *  und übergibt alle parameter an dieses weiter
+ *  
+ */
+public class ScriptExcecuter implements FOPRunnable {
+
+	public int runFop(String[] arg0) throws FOPException {
+		// Genug Parameter übergben?
+		if(arg0.length > 1){
+			File groovyScript = new File(arg0[1]);
+			// existiert die Datei ?
+			if(groovyScript.exists()){
+				// ist es eine Datei ?
+				if(groovyScript.isFile()){
+					try {
+						Binding binding = new Binding();
+						// Parameter weitergeben
+						binding.setVariable("arg", arg0);
+
+						// Imports festlegen damit diese nicht selbst hinzugefügt werden müssen
+						CompilerConfiguration cc = new CompilerConfiguration();
+						ImportCustomizer ic = new ImportCustomizer();
+						// abas Standard
+						ic.addStarImports("de.abas.eks.jfop.remote");
+						cc.addCompilationCustomizers(ic);
+						// Basisklasse festlegen
+						cc.setScriptBaseClass("de.finetech.groovy.AbasBaseScript");
+
+						// Script ausführen
+						GroovyShell shell = new GroovyShell(this.getClass().getClassLoader(), binding, cc);
+						shell.evaluate(groovyScript);
+						
+						return 0;
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					FO.box("Fehler", "Beim Ausführen des Scripts ist ein Fehler aufgetretten");
+				}else{
+					FO.box("Unzureichende Argumente", "Groovy Script ist keine Datei!");
+				}
+			}else{
+				FO.box("Unzureichende Argumente", "Groovy Script existiert nicht!");
+			}
+		}
+		else{
+			FO.box("Unzureichende Argumente", "keine Groovy Script angegeben!");
+		}
+		return -1;
+	}
+}
