518d630644dd582109f2fbd574bf15eef605c43e
Michael Kürbis <michael.kuerbis@gmx.de>
2016-03-29 10:23:26 +0200

    Update ScriptExcecuter.java

--- src/de/finetech/groovy/ScriptExcecuter.java
+++ src/de/finetech/groovy/ScriptExcecuter.java
@@ -15,16 +15,16 @@ import de.abas.eks.jfop.remote.FO;
 import de.abas.eks.jfop.remote.FOPRunnable;
 
 /**
- * @author Michael Kürbis, Finetech GmbH & Co. KG
+ * @author Michael KÃ¼rbis, Finetech GmbH & Co. KG
  * 
  *         JFOP erwartet als zweiten Parameter das Groovyscript welches
- *         ausgeführt werden soll und übergibt alle parameter an dieses weiter
+ *         ausgefÃ¼hrt werden soll und Ã¼bergibt alle parameter an dieses weiter
  * 
  */
 public class ScriptExcecuter implements FOPRunnable {
 
 	public int runFop(String[] arg0) throws FOPException {
-		// Genug Parameter übergben?
+		// Genug Parameter Ã¼bergben?
 		if (arg0.length > 1) {
 			File groovyScript = new File(arg0[1]);
 			// existiert die Datei ?
@@ -37,20 +37,22 @@ public class ScriptExcecuter implements FOPRunnable {
 						binding.setVariable("arg", arg0);
 
 						// Imports festlegen damit diese nicht selbst
-						// hinzugefügt werden müssen
+						// hinzugefÃ¼gt werden mÃ¼ssen
 						CompilerConfiguration cc = new CompilerConfiguration();
 						ImportCustomizer ic = new ImportCustomizer();
 						// abas Standard
 						ic.addStarImports("de.abas.eks.jfop.remote");
+						// ic.addStaticStars("de.abas.eks.jfop.remote.FO");
 						ic.addStarImports("de.finetech.groovy");
 						ic.addImports("java.awt.Color");
+						ic.addImports("de.finetech.utils.Infosystemcall", "de.finetech.utils.InfosystemcallResult");
 						// ic.addStaticImport("de.finetech.groovy.SelectionBuilder",
 						// "SelectionBuilder");
 						cc.addCompilationCustomizers(ic);
 						// Basisklasse festlegen
 						cc.setScriptBaseClass("de.finetech.groovy.AbasBaseScript");
 
-						// Script ausführen
+						// Script ausfÃ¼hren
 						GroovyShell shell = new GroovyShell(this.getClass()
 								.getClassLoader(), binding, cc);
 						shell.evaluate(groovyScript);
