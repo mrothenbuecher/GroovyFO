afe8d25a235a7595ecbb125fe4b1357d77dfc1db
Michael Kürbis <michael.kuerbis@gmx.de>
2016-03-29 10:26:15 +0200

    Create Tuple.java

---
+++ src/de/finetech/utils/Tuple.java
@@ -0,0 +1,29 @@
+package de.finetech.utils;
+
+public class Tuple <T,A> {
+
+	private T key;
+	private A value;
+	
+	public Tuple(T val1, A val2){
+		this.key = val1;
+		this.value = val2;
+	}
+
+	public T getKey() {
+		return key;
+	}
+
+	public void setKey(T key) {
+		this.key = key;
+	}
+
+	public A getValue() {
+		return value;
+	}
+
+	public void setValue(A value) {
+		this.value = value;
+	}
+	
+}
