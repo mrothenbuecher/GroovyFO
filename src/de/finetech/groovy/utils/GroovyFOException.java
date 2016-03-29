4133a1224091ad38a2a07c442c4dcd2fb0fac0fc
Michael Kürbis <michael.kuerbis@gmx.de>
2016-03-29 10:24:39 +0200

    Create GroovyFOException.java

---
+++ src/de/finetech/groovy/utils/GroovyFOException.java
@@ -0,0 +1,11 @@
+package de.finetech.groovy.utils;
+
+public class GroovyFOException extends Exception {
+
+	private static final long serialVersionUID = 4177094116089772597L;
+	
+	public GroovyFOException(String string) {
+		super(string);
+	}
+
+}
