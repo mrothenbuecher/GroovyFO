2e1020ed4708f8cd301b1e854c544084035c0b4f
Michael Kürbis <michael.kuerbis@gmx.de>
2016-06-09 13:59:10 +0200

    Bugfix

--- src/de/finetech/groovy/ScriptExcecuter.java
+++ src/de/finetech/groovy/ScriptExcecuter.java
@@ -3,15 +3,19 @@ package de.finetech.groovy;
 import groovy.lang.Binding;
 import groovy.lang.GroovyClassLoader;
 import groovy.lang.GroovyShell;
+import groovy.lang.GroovySystem;
 import groovy.lang.Script;
 
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.reflect.Field;
 
 import org.codehaus.groovy.control.CompilationFailedException;
 import org.codehaus.groovy.control.CompilerConfiguration;
 import org.codehaus.groovy.control.customizers.ImportCustomizer;
+import org.codehaus.groovy.runtime.metaclass.ConcurrentReaderHashMap;
+import org.codehaus.groovy.runtime.metaclass.MetaClassRegistryImpl;
 
 import de.abas.eks.jfop.AbortedException;
 import de.abas.eks.jfop.CommandException;
@@ -69,9 +73,16 @@ public class ScriptExcecuter implements ContextRunnable {
 						cc.setScriptBaseClass("de.finetech.groovy.AbasBaseScript");
 
 						// Script ausführen
-						shell = new GroovyShell(new GroovyClassLoader(), binding, cc);
+						GroovyClassLoader loader = new GroovyClassLoader();
+						shell = new GroovyShell(loader, binding, cc);
 						Script gscript = shell.parse(groovyScript);
-						gscript.run();
+						//gscript.run();
+						Object o = gscript.run();
+						if(o != null){
+							GroovySystem.getMetaClassRegistry().removeMetaClass(o.getClass());
+							GroovySystem.getMetaClassRegistry().removeMetaClass(gscript.getClass());
+						}
+						loader.clearCache();
 						shell = null;
 						error = false;
 					} catch (CommandException e) {
@@ -106,7 +117,7 @@ public class ScriptExcecuter implements ContextRunnable {
 								sw.toString());
 						error = true;
 					} finally {
-						System.gc();
+						clearGroovyClassesCache();
 						if(error){
 							return -2;
 						}
@@ -124,5 +135,39 @@ public class ScriptExcecuter implements ContextRunnable {
 			FO.box("Unzureichende Argumente", "keine Groovy Script angegeben!");
 		}
 		return -1;
+	}
+	
+	protected boolean clearGroovyClassesCache() {
+		try {
+			// Retrieve the fields we will manipulate
+			Class<MetaClassRegistryImpl> mcriClass = MetaClassRegistryImpl.class;
+
+			Field constantMetaClassesField = mcriClass
+					.getDeclaredField("constantMetaClasses");
+			constantMetaClassesField.setAccessible(true);
+
+			Field constantMetaClassCountField = mcriClass
+					.getDeclaredField("constantMetaClassCount");
+			constantMetaClassCountField.setAccessible(true);
+
+			// Retrieve the object to be manipulate
+			Class<GroovySystem> gsClass = GroovySystem.class;
+			Field metaClassRegistryField = gsClass
+					.getDeclaredField("META_CLASS_REGISTRY");
+			metaClassRegistryField.setAccessible(true);
+			MetaClassRegistryImpl mcri = (MetaClassRegistryImpl) metaClassRegistryField
+					.get(null);
+			ConcurrentReaderHashMap constantMetaClasses = (ConcurrentReaderHashMap) constantMetaClassesField
+					.get(mcri);
+
+			// Clears the map
+			constantMetaClasses.clear();
+			constantMetaClassCountField.set(mcri, 0);
+
+			return true;
+		} catch (Exception e) {
+			// Log this exception. It indicates that the code changed.
+			return false;
+		}
 	}
 }
